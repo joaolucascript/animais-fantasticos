621968c5555c6298a74bcba2a4583d26aac60ab1
João Lucas <zavarash@outlook.com>
2022-09-20 00:10:59 -0300

    refatorado ScrollSuave usando class

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 import tabNav from './modules/tabNav.js';
-import scrollSuave from './modules/scroll-suave.js';
+import ScrollSuave from './modules/scroll-suave.js';
 import animacaoScroll from './modules/animacaoScroll.js';
 import accordion from './modules/accordion.js';
 import modal from './modules/modal.js';
@@ -10,7 +10,9 @@ import funcionamento from './modules/funcionamento.js';
 import initFetchAnimais from './modules/fetch-animais.js';
 import initFetchBitcoin from './modules/fetch-bitcoin.js';
 
-scrollSuave();
+const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
+scrollSuave.init();
+
 animacaoScroll();
 accordion();
 tabNav();
