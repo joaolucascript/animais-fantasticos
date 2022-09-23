7fa03f6cf2ce874619bd49f5b609fd14aec9a717
João Lucas <zavarash@outlook.com>
2022-09-23 19:12:05 -0300

    adicionado slide de fotos

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ import MenuMobile from './modules/menu-mobile.js';
 import Funcionamento from './modules/funcionamento.js';
 import fetchAnimais from './modules/fetch-animais.js';
 import fetchBitcoin from './modules/fetch-bitcoin.js';
+import SlideNav from './modules/slide.js';
 
 const tabNav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section');
 tabNav.init();
@@ -39,3 +40,8 @@ funcionamento.init();
 
 fetchAnimais('./animais-api.json', '.numeros-grid');
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
+
+const slide = new SlideNav('.slide', '.wrapper');
+slide.init();
+
+slide.addControl('.custom-controls');
